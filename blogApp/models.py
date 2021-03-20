66cab21b42c03c5ab4efc867c3fa80a82d1c2ed7
Yaroslav Pidlisny <slavles04@gmail.com>
2021-03-20 19:31:57 +0200

    A lot of small features added, the project is done.

--- blogApp/models.py
+++ blogApp/models.py
@@ -6,8 +6,14 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=300)
     date = models.DateTimeField()
-    text = models.TextField(max_length=300)
+    text = models.TextField(max_length=700)
     image = models.ImageField(upload_to='event_images/')
 
     def get_summery(self):
-        return self.text[:70]
+        if len(self.text) > 70:
+            return self.text[:70] + '...'
+        else:
+            return self.text[:70]
+
+    def __str__(self):
+        return self.title
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
+
+
 def showblog(request):
     posts = Post.objects
     return render(request, 'blogApp/blog.html', {'posts': posts})
 
 
-
+def post_full(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'blogApp/post_full.html', {'post': post})
